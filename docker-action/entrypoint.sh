6c840c5956577a5277550198453c392d249a7175
Gabriel Knepper Mendes <gabriel666@gmail.com>
2024-01-30 09:43:04 -0300

    Update entrypoint.sh

--- docker-action/entrypoint.sh
+++ docker-action/entrypoint.sh
@@ -6,5 +6,11 @@ cd "${INPUT_WORKDIR:-.}"
 # Show Packer version
 packer version
 
+# Validate configuration
+packer ${INPUT_PACKERARGSVALIDATE}
+
+# Install plugins 
+packer ${INPUT_PACKERARGSINIT}
+
 # Run packer with the given arguments
-packer ${INPUT_PACKERARGS}
+packer ${INPUT_PACKERARGSBUILD}
